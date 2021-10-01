fc608103b51aad009d6ef9fc8268c35c9129c16b
Rodrigo Cibrian <cibrian@cpp.edu>
2021-09-30 23:45:46 -0700

    Initial outline for Guessing Game

---
+++ guessingGame.c
@@ -0,0 +1,39 @@
+/*
+    1. Create a menu with each game option
+        Press:
+            1 to play a gamePress 
+            2 to change the max numberPress 
+            3 to quit
+
+    2. If option 1 is selected ask user to enter a number guess between 1-10
+    3. Create a random number generator
+    4. Compare user input to random number using if else statment
+    5. if the number is correct print statement telling user they won
+    6. else compare numbers tell the user whether their guess was too low 
+        or too high
+    7.Prompt user to enter another guess or enter "q" to return to the main menu
+    
+
+    Option 2:
+    
+    1.Prompt user to enter the new max number
+    2. Set user input as new max number for games random number generator
+    3. Create a random number generator
+    4. Compare user input to random number using if else statment
+    5. if the number is correct print statement telling user they won
+    6. else compare numbers tell the user whether their guess was too low 
+        or too high
+    7.Prompt user to enter another guess or enter "q" to return to the main menu
+
+    Option 3:
+    
+    Thank User for Playing and end the game.
+
+    Create a new branch called save_user_max_number
+    Save the value to a seperate .txt file
+    
+    Have the program check if .txt file contains a vaulue and utilize for rand max
+    
+
+
+*/
